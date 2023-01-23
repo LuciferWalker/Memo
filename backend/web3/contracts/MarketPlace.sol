3eb54bb469f7942e892e9174f5fb6c8aa776dbb4
Lucifer Walker <preetpatel1616@gmail.com>
2023-01-23 20:09:08 +0530

    Added MarketPlace contract code

--- backend/web3/contracts/MarketPlace.sol
+++ backend/web3/contracts/MarketPlace.sol
@@ -1,37 +1,62 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "./AccessToken.sol";
 
 contract MarketPlace {
 
-    uint projectCounter;
+    using Counters for Counters.Counter;
+    Counters.Counter private projectCounter;
 
     //add creators to list to distribute royalties
 
+    uint private projectCost;
     address payable memo;
 
     struct Project{
         uint projectId;
-
+        string CID;
+        AccessToken tokenAddress;
     }
+
 
     mapping (uint => Project) public projectIdToDetails;
 
 
-    constructor() {
+    event ProjectCreated(uint indexed projectId, string CID, AccessToken tokenAddress);
+
+    constructor(uint _projectCost) {
         memo = payable(msg.sender);
+        projectCost = _projectCost;
     }
 
-    function createProject(string memory name, string memory symbol, uint maxSupply, uint tokenPrice) public payable{
-        require(msg.value > 2 ether, "Insufficient Amount");
+
+    function createProject(string memory name, string memory symbol, string memory CID, uint maxSupply, uint tokenPrice, address[] memory creators, uint[] memory shares) public payable{
+        require(msg.value >= projectCost, "Insufficient Amount");
         memo.transfer(msg.value);
 
         //deploy the access token
+        AccessToken newAccessTokenContract = new AccessToken(name, symbol, maxSupply, tokenPrice, creators, shares);
+
+        uint currentId = projectCounter.current();
+        projectCounter.increment();
+        projectIdToDetails[currentId] = Project(
+            currentId,
+            CID,
+            newAccessTokenContract
+        );
+        emit ProjectCreated(currentId, CID, newAccessTokenContract);
     }
     
-
     function getProjects() public view {
         //returns the projectCounter value. Then in front end we can run a loop till that id
     }
+
+    function buyProjectToken(uint _projectId) public payable {
+        require(_projectId < projectCounter.current(), "Invalid Project Id");
+        // Calls mint function of the token
+        projectIdToDetails[_projectId].tokenAddress.mint();
+    }
+
 }
