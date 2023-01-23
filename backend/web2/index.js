4db2efd840773ec53886d25028b06aaf6fa556b0
Lucifer Walker <preetpatel1616@gmail.com>
2023-01-23 23:03:09 +0530

    added some logic in deploy.js and index.js in backend folder. Created contractsData in frontend/src

---
+++ backend/web2/index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+require("dotenv".config({ path: "./config.env" }));
+const PORT = process.env.PORT;
+
+require("./database/connection");
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/uploadFile", async (req, res) => {
+  //upload file to the ipfs, then upload the contract
+  //upload file logic
+});
+
+app.post("/createProject", async (req, res) => {
+  const {
+    projectTitle,
+    projectDescription,
+    creators,
+    tokenPrice,
+    tokenSupply,
+    shares,
+  } = req.body;
+
+  //store data in database
+
+  //call createProject function from marketplace
+
+});
+
+app.get("/listedProjects", async(req,res) => {
+
+    //fetch all projects whos status is true from mongodb
+})
+
+app.post("/downloadFile", async(req,res) => {
+
+    const {projectId} = req.body
+
+    //download the content using the cid associated with this projectId
+})
+
+app.get("/boughtProjects", async(req,res)=>{
+
+    //show all projects the user has bought tokens in
+})
+
+app.get("/createdProjects", async(req,res)=>{
+
+    //show all projects the user has created
+})
+
+
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
