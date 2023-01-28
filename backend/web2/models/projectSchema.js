94f1b51c1df6130256319605aa0100521f422362
nitish@gaga <nitishgoku01@gmail.com>
2023-01-28 18:12:13 +0530

    added timestamp on projectSchema

--- backend/web2/models/projectSchema.js
+++ backend/web2/models/projectSchema.js
@@ -1,60 +1,63 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-const ProjectSchema = mongoose.Schema({
-  projectId: {
-    type: Number,
-    required: true,
+const ProjectSchema = mongoose.Schema(
+  {
+    projectId: {
+      type: Number,
+      required: true,
+    },
+    image: {
+      type: String,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    tokenContractAddress: {
+      type: String,
+      required: true,
+    },
+    creatorAddresses: {
+      type: [String],
+      required: true,
+    },
+    creatorShares: {
+      type: [Number],
+      required: true,
+    },
+    cid: {
+      type: String,
+      required: true,
+    },
+    projectStatus: {
+      type: Boolean,
+      default: true,
+    },
+    fileSize: {
+      type: Number,
+    },
+    fileName: {
+      type: String,
+    },
+    totalTokens: {
+      type: Number,
+      required: true,
+    },
+    tokensBought: {
+      type: Number,
+      default: 0,
+    },
+    tokenPrice: {
+      type: Number,
+      required: true,
+    },
   },
-  image: {
-    type: String,
-  },
-  title: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  tokenContractAddress: {
-    type: String,
-    required: true,
-  },
-  creatorAddresses: {
-    type: [String],
-    required: true, 
-  },
-  creatorShares:{
-    type: [Number],
-    required: true, 
-  },
-  cid: {
-    type: String,
-    required: true,
-  },
-  projectStatus: {
-    type: Boolean,
-    default: true,
-  },
-  fileSize: {
-    type: Number,
-  },
-  fileName: {
-    type: String,
-  },
-  totalTokens: {
-    type: Number,
-    required: true,
-  },
-  tokensBought: {
-    type: Number,
-    default: 0,
-  },
-  tokenPrice: {
-    type: Number,
-    required: true,
-  },
-});
+  { timestamps: true },
+)
 
-const Project = mongoose.model("Project", ProjectSchema);
-module.exports = Project;
+const Project = mongoose.model('Project', ProjectSchema)
+module.exports = Project
