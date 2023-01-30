6de672cacefa10ac5e77b7254d875a7a777871eb
nitish@gaga <nitishgoku01@gmail.com>
2023-01-30 10:16:55 +0530

    fixing routes and modifying schema

--- backend/web2/models/projectSchema.js
+++ backend/web2/models/projectSchema.js
@@ -10,7 +10,7 @@ const ProjectSchema = mongoose.Schema(
       type: String,
       required: true,
     },
-    projectSymbol: {
+    tokenSymbol: {
       type: String,
       required: true,
     },
