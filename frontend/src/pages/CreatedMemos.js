a7c9e1dab58afc227d1eba8b7a8603842623232b
Lucifer Walker <preetpatel1616@gmail.com>
2023-02-03 19:23:31 +0530

    updates

--- frontend/src/pages/CreatedMemos.js
+++ frontend/src/pages/CreatedMemos.js
@@ -30,7 +30,7 @@ const CreatedMemos = () => {
 
   return (
     <>
-      {createdProjects?.length == 0 (
+      {createdProjects?.length == 0 ? (
         <div style={{ marginLeft: '400px', marginTop: '120px' }}>
           <Spinner color="white" size={100} />
         </div>
