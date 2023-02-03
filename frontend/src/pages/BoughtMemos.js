3e7e5775bea9030f38a69a2f2179657856825975
nitish@gaga <nitishgoku01@gmail.com>
2023-02-03 18:47:39 +0530

    fixing dashboard

--- frontend/src/pages/BoughtMemos.js
+++ frontend/src/pages/BoughtMemos.js
@@ -1,60 +1,96 @@
-import { useNavigate } from "react-router-dom";
+import { useState, useEffect, useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { MemoContext } from '../context/MemoContext'
+import { NotificationManager } from 'react-notifications'
+import Spinner from 'react-spinners/ClipLoader'
+import { BASE_URL, formatBytes } from '../utils'
 
-const BoughtMemos = (props) => {
-  const navigate = useNavigate();
+const BoughtMemos = () => {
+  const navigate = useNavigate()
+  const [boughtProjects, setBoughtProjects] = useState(null)
+  const { account: userAddress } = useContext(MemoContext)
+
+  const getBoughtProjects = async (userAddress) => {
+    const response = await fetch(
+      `${BASE_URL}/boughtProjects?address=${userAddress}`,
+    )
+    const projects = await response.json()
+    setBoughtProjects(projects)
+  }
+
+  useEffect(() => {
+    getBoughtProjects(userAddress)
+
+    if (!userAddress) {
+      navigate('/')
+      NotificationManager.info('Connect Your Wallet!', 'Warning', 3000)
+    }
+  }, [])
+  console.log(boughtProjects)
+
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        height: "440px",
-        overflowY: "auto",
-      }}
-    >
-      <h2>BOUGHT PROJECTS</h2>
-      {props.project == null ? (
-        <h3 style={{ color: "white" }}>No Projects Bought, yet &#59;&#41;</h3>
+    <>
+      {!boughtProjects ? (
+        <div style={{ marginLeft:'400px', marginTop: '120px' }}>
+          <Spinner color="white" size={100} />
+        </div>
       ) : (
-        props.project?.map((card, index) => (
-          <>
-            <table
-              key={index}
-              onClick={() => navigate(`/explore/${card.projectId}`)}
-              style={{
-                background:
-                  "linear-gradient(180deg, rgba(0, 27, 96, 0.7) 0%, rgba(127, 1, 1, 0) 100%)",
-                padding: "0px",
-              }}
-            >
-              <tr>
-                <td style={{ paddingLeft: "40px" }}>
-                  <h5>{card.projectName}</h5>
-                </td>
-                <td style={{ paddingLeft: "80px" }}></td>
-                <td>
-                  <h5>File Size: {card.fileSize}</h5>
-                </td>
-              </tr>
-              <tr>
-                <td style={{ width: "340px", paddingLeft: "40px" }}>
-                  <h5>{card.Description}</h5>
-                  <h5>{card.Date || ""}</h5>
-                </td>
-                <td style={{ width: "270px" }}></td>
-                <td style={{ width: "200px" }}>
-                  <h5>
-                    {" "}
-                    PRICE: {card.tokenPrice} FIL
-                    <br /> $ DOWNLOAD
-                  </h5>
-                </td>
-              </tr>
-            </table>
-            <br />
-          </>
-        ))
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            height: '440px',
+            overflowY: 'auto',
+          }}
+        >
+          <h2>BOUGHT PROJECTS</h2>
+          {boughtProjects == null ? (
+            <h3 style={{ color: 'white' }}>
+              No Projects Bought, yet &#59;&#41;
+            </h3>
+          ) : (
+            boughtProjects?.map((card, index) => (
+              <>
+                <table
+                  key={index}
+                  onClick={() => navigate(`/explore/${card.projectId}`)}
+                  style={{
+                    background:
+                      'linear-gradient(180deg, rgba(0, 27, 96, 0.7) 0%, rgba(127, 1, 1, 0) 100%)',
+                    padding: '0px',
+                  }}
+                >
+                  <tr>
+                    <td style={{ paddingLeft: '40px' }}>
+                      <h5>{card.projectName}</h5>
+                    </td>
+                    <td style={{ paddingLeft: '80px' }}></td>
+                    <td>
+                      <h5>File Size: {formatBytes(card.fileSize)}</h5>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td style={{ width: '340px', paddingLeft: '40px' }}>
+                      <h5>{card.Description}</h5>
+                      <h5>{card.Date || ''}</h5>
+                    </td>
+                    <td style={{ width: '270px' }}></td>
+                    <td style={{ width: '200px' }}>
+                      <h5>
+                        {' '}
+                        PRICE: {card.tokenPrice} FIL
+                        <br /> $ DOWNLOAD
+                      </h5>
+                    </td>
+                  </tr>
+                </table>
+                <br />
+              </>
+            ))
+          )}
+        </div>
       )}
-    </div>
-  );
-};
-export default BoughtMemos;
+    </>
+  )
+}
+export default BoughtMemos
--- frontend/src/pages/CreatedMemos.js
+++ frontend/src/pages/CreatedMemos.js
@@ -1,59 +1,51 @@
-import { useState, useEffect, useContext } from "react";
-import { useNavigate } from "react-router-dom";
-import { MemoContext } from "../context/MemoContext";
-import { NotificationManager } from "react-notifications";
-import { BASE_URL } from "../utils";
+import { useState, useEffect, useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { MemoContext } from '../context/MemoContext'
+import { NotificationManager } from 'react-notifications'
+import Spinner from 'react-spinners/ClipLoader'
+import { BASE_URL, formatBytes } from '../utils'
 
 const CreatedMemos = () => {
-  const navigate = useNavigate();
-
-  const [creatememo, setCreateMemo] = useState(true);
-  const [createdProjects, setCreatedProjects] = useState(null);
-  const [boughtProjects, setBoughtProjects] = useState(null);
-
-  const { account: userAddress } = useContext(MemoContext);
+  const navigate = useNavigate()
+  const [createdProjects, setCreatedProjects] = useState(null)
+  const { account: userAddress } = useContext(MemoContext)
 
   const getCreatedProjects = async (userAddress) => {
     const response = await fetch(
-      `${BASE_URL}/createdProjects?address=${userAddress}`
-    );
-    const projects = await response.json();
-    setCreatedProjects(projects);
-  };
-
-  const getBoughtProjects = async (userAddress) => {
-    const response = await fetch(
-      `${BASE_URL}/boughtProjects?address=${userAddress}`
-    );
-    const projects = await response.json();
-    setBoughtProjects(projects);
-  };
+      `${BASE_URL}/createdProjects?address=${userAddress}`,
+    )
+    const projects = await response.json()
+    setCreatedProjects(projects)
+  }
 
   useEffect(() => {
-    getCreatedProjects(userAddress);
-    getBoughtProjects(userAddress);
+    getCreatedProjects(userAddress)
 
     if (!userAddress) {
-      navigate("/");
-      NotificationManager.info("Connect Your Wallet!", "Warning", 3000);
+      navigate('/')
+      NotificationManager.info('Connect Your Wallet!', 'Warning', 3000)
     }
-  }, []);
-
+  }, [])
+  console.log(createdProjects)
 
   return (
     <>
-      {creatememo && (
+      {!createdProjects ? (
+        <div style={{ marginLeft: '400px', marginTop: '120px' }}>
+          <Spinner color="white" size={100} />
+        </div>
+      ) : (
         <div
           style={{
-            display: "flex",
-            flexDirection: "column",
-            height: "440px",
-            overflowY: "auto",
+            display: 'flex',
+            flexDirection: 'column',
+            height: '440px',
+            overflowY: 'auto',
           }}
         >
           <h2>CREATED PROJECTS</h2>
           {createdProjects?.length == 0 ? (
-            <h3 style={{ color: "white" }}>
+            <h3 style={{ color: 'white' }}>
               No Projects Created, yet &#59;&#41;
             </h3>
           ) : (
@@ -64,28 +56,28 @@ const CreatedMemos = () => {
                   onClick={() => navigate(`/explore/${card.projectId}`)}
                   style={{
                     background:
-                      "linear-gradient(180deg, rgba(0, 27, 96, 0.7) 0%, rgba(127, 1, 1, 0) 100%)",
-                    padding: "0px",
+                      'linear-gradient(180deg, rgba(0, 27, 96, 0.7) 0%, rgba(127, 1, 1, 0) 100%)',
+                    padding: '0px',
                   }}
                 >
                   <tr>
-                    <td style={{ paddingLeft: "40px" }}>
+                    <td style={{ paddingLeft: '40px' }}>
                       <h5>{card.projectName}</h5>
                     </td>
-                    <td style={{ paddingLeft: "80px" }}></td>
+                    <td style={{ paddingLeft: '80px' }}></td>
                     <td>
-                      <h5>File Size: {card.fileSize}</h5>
+                      <h5>File Size: {formatBytes(card.fileSize)}</h5>
                     </td>
                   </tr>
                   <tr>
-                    <td style={{ width: "340px", paddingLeft: "40px" }}>
+                    <td style={{ width: '340px', paddingLeft: '40px' }}>
                       <h5>{card.projectDescription}</h5>
-                      <h5>{card.Date || ""}</h5>
+                      <h5>{card.Date || ''}</h5>
                     </td>
-                    <td style={{ width: "270px" }}></td>
-                    <td style={{ width: "200px" }}>
+                    <td style={{ width: '270px' }}></td>
+                    <td style={{ width: '200px' }}>
                       <h5>
-                        {" "}
+                        {' '}
                         PRICE: {card.tokenPrice} FIL
                         <br /> $ DOWNLOAD
                       </h5>
@@ -99,6 +91,6 @@ const CreatedMemos = () => {
         </div>
       )}
     </>
-  );
-};
-export default CreatedMemos;
+  )
+}
+export default CreatedMemos
